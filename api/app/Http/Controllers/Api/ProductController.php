58e30e6f63c4d1c54bda129fd69f4ff905c54f5f
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-01-06 17:57:55 -0500

    Add show method in ProductController.php

--- api/app/Http/Controllers/Api/ProductController.php
+++ api/app/Http/Controllers/Api/ProductController.php
@@ -42,7 +42,8 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
+        $product = Product::find($id);
+        return $product;
     }
 
     /**
