890d747fb596ef626503109c8fb8236238409b5a
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-01-08 12:04:51 -0500

    Add components to App

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ShowProducts from "./components/ShowProducts.jsx";
+import CreateProduct from "./components/CreateProduct.jsx";
+import EditProduct from "./components/EditProduct.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-<button type="button" className={'btn btn-primary'}>Add product</button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ShowProducts />} />
+          <Route path="/create" element={<CreateProduct />} />
+          <Route path="/edit/:id" element={<EditProduct />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
