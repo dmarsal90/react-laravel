f9baad843fae969ba02e2c7a58c870f24f26d123
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-03-14 09:25:41 -0400

    Update routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -19,11 +19,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::
-controller(ProductController::class)->group(function(){
-    Route::get('/products', 'index');
-    Route::post('/product', 'store');
-    Route::get('/product/{id}', 'show');
-    Route::put('/product/{id}', 'update');
-    Route::delete('/product/{id}', 'destroy');
-});
+Route::controller(ProductController::class)->group(function () {
+        Route::get('/products', 'index');
+        Route::post('/product', 'store');
+        Route::get('/product/{id}', 'show');
+        Route::put('/product/{id}', 'update');
+        Route::delete('/product/{id}', 'destroy');
+    });
