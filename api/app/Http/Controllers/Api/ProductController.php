97f08d60938c43142d68ba94e26837e87f3a1d08
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-03-14 09:11:04 -0400

    Update store method

--- api/app/Http/Controllers/Api/ProductController.php
+++ api/app/Http/Controllers/Api/ProductController.php
@@ -22,7 +22,12 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $product = new Product();
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+
+        $product->save();
     }
 
     /**
