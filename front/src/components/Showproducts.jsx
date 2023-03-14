b49540cd4771911c58a107746aa6392b5de7aec4
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-03-14 10:48:52 -0400

    Update ShowProduct

--- front/src/components/Showproducts.jsx
+++ front/src/components/Showproducts.jsx
@@ -23,6 +23,42 @@ const Showproducts = () => {
   return (
     <div>
       <h1>Products</h1>
+      <div className="d-grid gap-2">
+        <Link
+          to="/create"
+          className="btn btn-success btn-lg mt-2 mb-2 text-white "
+        >
+          Create
+        </Link>
+      </div>
+      <table className="table table-striped">
+        <thead className="bg-primary text-white">
+          <tr>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Stock</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.description}</td>
+              <td>{product.price}</td>
+              <td>{product.stock}</td>
+              <td>
+                <Link
+                  to={`/edit/product/${product.id}`}
+                  className="btn btn-warning"
+                >
+                  Edit
+                </Link>
+                <button onClick={()=> deleteProduct(product.id)} className="btn btn-danger">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
