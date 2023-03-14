7cddb98f3a043fa7da332239938b8095d8338c58
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-03-14 12:04:07 -0400

    Update CreateProduct component

---
+++ front/src/components/CreateProduct.jsx
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+const CreateProduct = () => {
+  return (
+    <div>
+      <h1>Add Product</h1>
+    </div>
+  )
+}
+
+export default CreateProduct
