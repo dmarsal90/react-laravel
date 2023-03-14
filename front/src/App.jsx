94d3ee16468efda2cb88fa40f346283b7fb12829
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-03-14 10:56:37 -0400

    Add Showproducts component to App

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Showproducts from "./components/Showproducts";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Showproducts/>}/>
+        </Routes>
+      </BrowserRouter>
       
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
