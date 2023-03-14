a5064becf2367261a90387fd43a04e8d1db7d7a5
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-03-14 10:06:28 -0400

    Add getAllProducts and deleteProduct functions

--- front/src/components/Showproducts.jsx
+++ front/src/components/Showproducts.jsx
@@ -2,10 +2,20 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
 
+
 const Showproducts = () => {
+
+    const getAllProducts = () => {
+
+    }
+
+    const deleteProduct = () => {
+        
+    }
+
   return (
     <div>
-      
+      <h1>Products</h1>
     </div>
   )
 }
