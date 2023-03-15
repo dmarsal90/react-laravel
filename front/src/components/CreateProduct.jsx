a41f9e0614be9535604cba4d8038e600dad2c8a4
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-03-15 12:35:27 -0400

    Update store method

--- front/src/components/CreateProduct.jsx
+++ front/src/components/CreateProduct.jsx
@@ -1,19 +1,26 @@
-import React,{useState} from 'react'
+import React, {useState} from 'react'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 
 const endpoint = 'http://localhost:8000/api/product'
 
 const CreateProduct = () => {
 
-    const store = () => {
-        
+    const [description, setDescription] = useState('')
+    const [price, setPrice] = useState(0)
+    const [stock, setStock] = useState(0)
+    const navigate = useNavigate()
+
+    const store = async (e) => {
+        e.preventDefault()
+        await axios.post(endpoint, {description: description, price: price, stock: stock})
+        navigate('/')
     }
-  return (
-    <div>
-      <h1>Add Product</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Add Product</h1>
+        </div>
+    )
 }
 
 export default CreateProduct
