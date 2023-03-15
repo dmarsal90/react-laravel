7df35e136d0a885cc47b3942ad0cb8fabc01886d
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-03-15 15:10:42 -0400

    Update EditProduct.jsx

--- front/src/components/EditProduct.jsx
+++ front/src/components/EditProduct.jsx
@@ -4,6 +4,11 @@ import {useNavigate, useParams} from "react-router-dom";
 
 const endpoint = "http://localhost:8000/api/product";
 const EditProduct = () => {
+    const [description, setDescription] = useState('')
+    const [price, setPrice] = useState(0)
+    const [stock, setStock] = useState(0)
+    const navigate = useNavigate()
+    const {id} = useParams()
     return (
         <div>
             <h1>Edit Product</h1>
