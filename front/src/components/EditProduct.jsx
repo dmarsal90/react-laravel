4a2db1564a4551fcd8b8800270713de1362098c6
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-03-15 15:08:52 -0400

    Add endpoint

--- front/src/components/EditProduct.jsx
+++ front/src/components/EditProduct.jsx
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react';
+import axios from "axios";
+import {useNavigate, useParams} from "react-router-dom";
 
-
+const endpoint = "http://localhost:8000/api/product";
 const EditProduct = () => {
     return (
         <div>
-
+            <h1>Edit Product</h1>
         </div>
     );
 
