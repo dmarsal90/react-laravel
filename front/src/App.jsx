99dd271120d0e8969be11c361b53768f2390e42b
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-03-15 22:29:51 -0400

    Add EditProduct component

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,22 +1,24 @@
-import { useState } from "react";
+import {useState} from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import ShowProducts from "./components/Showproducts";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import CreateProduct from "./components/CreateProduct.jsx";
+import EditProduct from "./components/EditProduct.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<ShowProducts/>}/>
-            <Route path='/create' element={<CreateProduct/>}/>
-        </Routes>
-      </BrowserRouter>
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<ShowProducts/>}/>
+                    <Route path='/create' element={<CreateProduct/>}/>
+                    <Route path='/edit/:id' element={<EditProduct/>}/>
+                </Routes>
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
 export default App;
